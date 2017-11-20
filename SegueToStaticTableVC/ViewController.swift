e6d1a04f84fe581968e8ac059f7ff0e8ba501e96
HackingGate <hackinggate@icloud.com>
2017-11-20 23:59:25 +0900

    Initial commit

---
+++ SegueToStaticTableVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SegueToStaticTableVC
+//
+//  Created by ERU on 2017/11/20.
+//  Copyright © 2017年 ERU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
